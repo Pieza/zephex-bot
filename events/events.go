2c91cebaf73aae7a1a30ebdd318c4635561a9885
Jose Ulloa <jandres258@hotmail.com>
2020-07-07 22:34:32 -0600

    image that works

--- events/events.go
+++ events/events.go
@@ -14,7 +14,7 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "manda huevo" {
 		embed := &discordgo.MessageEmbed{
 		    Image: &discordgo.MessageEmbedImage {
-					URL: "https://cdn.discordapp.com/avatars/119249192806776836/cc32c5c3ee602e1fe252f9f595f9010e.jpg?size=2048",
+					URL: "https://github.com/pieza/zephex-bot/blob/master/assets/images/zephex.jpeg?raw=true",
 				},
 		}
 		s.ChannelMessageSendEmbed(m.ChannelID, embed)
