bc10df77861e191579c795f103c6c6886ff2a41d
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-07-10 23:12:17 -0600

    manda huevo pieza

--- events/events.go
+++ events/events.go
@@ -12,7 +12,7 @@ func Ready(s *discordgo.Session, event *discordgo.Ready) {
 }
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {	
-	if strings.ToLower(m.Content) == "manda huevo" || strings.Contains(strings.ToLower(m.Content), "manda huevo") {
+	if strings.Contains(strings.ToLower(m.Content), "manda huevo") {
 		embed := &discordgo.MessageEmbed{
 		    Image: &discordgo.MessageEmbedImage {
 					URL: "https://github.com/pieza/zephex-bot/blob/master/assets/images/zephex.jpeg?raw=true",
